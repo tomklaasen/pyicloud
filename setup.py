fd6d42221956bb573d49a17c894c56f67021416e
Anders Johansson <mejlaandersj@gmail.com>
2019-06-28 11:06:29 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pyicloud_ipd',
-    version='0.10.0',
+    version='0.11.0',
     url='https://github.com/ndbroadbent/pyicloud',
     description=(
         'PyiCloud is a module which allows pythonistas to '
